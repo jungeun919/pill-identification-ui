63f4ea52325e14726528a09c0c14f08d5476074a
jungeun919 <renee8369@likelion.org>
2023-05-15 17:35:00 +0900

    medicine list check

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-function App() {
+function MedicineList() {
+  const [medicines, setMedicines] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:8000/api/medicine/").then((response) => {
+      setMedicines(response.data);
+    });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Medicine List</h1>
+      <ul>
+        {medicines.map((medicine) => (
+          <li key={medicine.id}>
+            {medicine.product_name}
+            {medicine.manufacturer_name}
+            {medicine.appearance}
+            {medicine.length}
+            {medicine.width}
+            {medicine.thickness}
+            {medicine.appearance}
+            {medicine.category_name}
+            {medicine.specialized_general_name}
+            {medicine.dosage_form_name}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
 
-export default App;
+export default MedicineList;
