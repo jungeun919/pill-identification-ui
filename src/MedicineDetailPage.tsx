aba6d2cb6e22327942b8c600b9e261a0ead02feb
minsekim <tkarnrwl7862@naver.com>
2023-06-19 01:18:44 +0900

    더미데이터 삭제 & 데이터 중복 삭제

--- src/MedicineDetailPage.tsx
+++ src/MedicineDetailPage.tsx
@@ -3,17 +3,6 @@ import "./App.css";
 import { MedicineType } from "./WebcamPage";
 
 const MedicineDetailPage = () => {
-  // 더미 데이터
-  const product_name = "dummy_product_name1";
-  const manufacturer_name = "dummy_manufacturer_name1";
-  const appearance = "dummy_appearance1";
-  const length = "dummy_length1";
-  const width = "dummy_width1";
-  const thickness = "dummy_thickness1";
-  const category_name = "dummy_category_name1";
-  const specialized_general_name = "dummy_specialized_general_name1";
-  const dosage_form_name = "dummy_dosage_form_name1";
-
   const [medicine, setMedicine] = useState<MedicineType | null>(null);
 
   useEffect(() => {
@@ -26,8 +15,11 @@ const MedicineDetailPage = () => {
   }
 
   return (
-    <div>
+    <div style={{ paddingLeft: "5vw" }}>
       <h1>Medicine Details</h1>
+      {medicine.product_image && (
+        <img src={medicine.product_image} width="500" alt="알약이미지" style={{ borderRadius: 2 }} />
+      )}
       <h2>{medicine.product_name}</h2>
       <p>Manufacturer: {medicine.manufacturer_name}</p>
       <p>Appearance: {medicine.appearance}</p>
